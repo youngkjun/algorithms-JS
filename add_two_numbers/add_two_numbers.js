11b6c0dd1788c9c7c05eecb5a135e54018c99f1e
Young Kyun Jun <young.k.jun@gmail.com>
2018-10-29 14:15:34 -0400

    Add two numbers

--- add_two_numbers/add_two_numbers.js
+++ add_two_numbers/add_two_numbers.js
@@ -1,17 +1,15 @@
 /**
+ * Add Two Numbers
+ * 
  * Proverbs 3:5-7
  * Trust in the LORD with all your heart and lean not on your own understanding;
  * in all your ways submit to him, and he will make your paths straight.
  * Do not be wise in your own eyes; fear the LORD and shun evil.
- */
-
-/**
- * Add Two Numbers
  * 
- * You are given two non-empty linked lists representing two non-negative integers. 
- * The digits are stored in reverse order and each of their nodes contain a single digit. 
- * Add the two numbers and return it as a linked list.
- * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 Example:
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
